2ba9d392281d98e4f56dbe54374a56ada5f5ad89
Marek294 <Markra5835@gmail.com>
2022-06-25 10:40:47 +0200

    install sass, move app to src dir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
+  publicRuntimeConfig: {
+    appServerUrl: process.env.APP_SERVER_URL
+  }
 }
 
 module.exports = nextConfig
